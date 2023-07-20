665f8945eff7584b31a85fc6972dbef8f7dc92af
Radionom <135643863+Radionom@users.noreply.github.com>
2023-07-20 22:59:20 +0000

    Updated the qgis feedstock.

--- packages/foundry/src/foundry.ts
+++ packages/foundry/src/foundry.ts
@@ -7,3 +7,12 @@ import {
     validateFunction,
 } from '@usefoundry/utils'
 
+export class Foundry {
+    private tools: object[] = []
+    private flatFunctions: ParsedFunctionRef[] = []
+
+    constructor({ tools }: { tools: (object | FunctionRef)[] }) {
+        for (const entity of tools) {
+            // if it's a function
+            if (typeof entity === 'function') {
+                if (!validate
