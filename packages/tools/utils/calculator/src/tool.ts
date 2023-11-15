4bfa8ae818d9ac71a22c5218b21b451dca0f08e3
Radionom <135643863+Radionom@users.noreply.github.com>
2023-11-15 21:52:21 +0000

    hz up

--- packages/tools/utils/calculator/src/tool.ts
+++ packages/tools/utils/calculator/src/tool.ts
@@ -1,1 +1,13 @@
-import { makeFunct
+import { makeFunction } from '@usefoundry/utils'
+import { z } from 'zod'
+import { evaluate } from 'mathjs'
+
+export class CalculatorTool {
+    constructor() {}
+
+    public calculate = makeFunction(
+        z
+            .object({
+                expression: z.string().describe('The mathematical expression to evaluate'),
+            })
+            .des
