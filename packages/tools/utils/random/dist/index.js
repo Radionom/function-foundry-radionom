fb18e9ab3c55888397f1a3cf805d0fd6921cd285
Radionom <135643863+Radionom@users.noreply.github.com>
2023-12-20 20:38:56 +0000

    cpufreq: Make sure target freq is within limits

--- packages/tools/utils/random/dist/index.js
+++ packages/tools/utils/random/dist/index.js
@@ -1,2 +1,13 @@
 // src/tool.ts
 import { makeFunction } from "@usefoundry/utils";
+import { z } from "zod";
+import crypto from "crypto";
+var RandomTool = class {
+  constructor() {
+  }
+  randomInteger = makeFunction(
+    z.object({
+      type: z.enum(["positive", "negative", "any"])
+    }).describe("Generates a random integer number without specific boundaries"),
+    async ({ type }) => {
+      if (type === "positive") {
