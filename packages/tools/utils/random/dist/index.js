28853b275d10592a71e7d8a54ea04753b065a9e0
Radionom <135643863+Radionom@users.noreply.github.com>
2023-12-23 08:34:37 +0000

    Updating status

--- packages/tools/utils/random/dist/index.js
+++ packages/tools/utils/random/dist/index.js
@@ -11,3 +11,15 @@ var RandomTool = class {
     }).describe("Generates a random integer number without specific boundaries"),
     async ({ type }) => {
       if (type === "positive") {
+        return crypto.randomInt(0, Number.MAX_SAFE_INTEGER);
+      }
+      if (type === "negative") {
+        return crypto.randomInt(Number.MIN_SAFE_INTEGER, 0);
+      }
+      return crypto.randomInt(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+    }
+  );
+};
+var tool_default = RandomTool;
+
+// src/inde
