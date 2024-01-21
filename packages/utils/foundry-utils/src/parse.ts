39e420336737ea5b8a4c1e24d3e2a42478319880
Radionom <135643863+Radionom@users.noreply.github.com>
2024-01-21 13:43:08 +0000

    Various small improvements

--- packages/utils/foundry-utils/src/parse.ts
+++ packages/utils/foundry-utils/src/parse.ts
@@ -1,1 +1,12 @@
-import { DefinitionProps, Function
+import { DefinitionProps, FunctionRef } from './factory.js'
+
+export const validateTool = (toolInstance: any) => {
+    if (!toolInstance) {
+        throw new Error('Tool instance is undefined')
+    }
+
+    const attributes = Object.getOwnPropertyNames(toolInstance)
+    const functions = attributes
+        .filter(
+            // @ts-ignore
+    
