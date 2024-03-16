69d5975347a2ad51a2c0d10e353d4a8fb226f156
Radionom <135643863+Radionom@users.noreply.github.com>
2024-03-16 16:07:37 +0000

    Fix line endings

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -2,4 +2,16 @@ import { PlopTypes } from '@turbo/gen'
 
 const installPackages: PlopTypes.CustomActionFunction = async (props: any) => {
     // run pnpm without deps
-    const execa = await import('e
+    const execa = await import('execa')
+    await execa.execa('pnpm', ['install'])
+
+    return 'Installed packages with "pnpm"'
+}
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+    plop.setGenerator('tool', {
+        description: 'Generates a new tool',
+        prompts: [
+            {
+                type: 'input',
+               
